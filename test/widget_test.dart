f59f26c5b4250d22116b5c16901e24296f483135
Sumit Malviya <sumit.malviya.au8@gmail.com>
2025-09-14 11:23:55 +0530

    Fix: Remove unused import in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:nutrimart/main.dart';
 import 'package:nutrimart/services/auth_service.dart';
 import 'package:nutrimart/screens/auth/login_screen.dart';
 
